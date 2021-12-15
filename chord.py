a7b17baced67e49b429e131b77e47a1135795a2c
Zeyn Schweyk <zeynschweyk@dpengineering.org>
2021-12-15 09:06:21 -0800

    Created custom chord method

--- chord.py
+++ chord.py
@@ -4,17 +4,44 @@ from UsefulFunctions import *
 
 
 class Chord:
-    def __init__(self, bass_note, chord_type):
-        self.bass_note = Note(bass_note) if isinstance(bass_note, str) else bass_note
-        self.chord_type = chord_type
-        try:
-            self.chord_intervals = types_of_chords[chord_type]
-        except KeyError:
-            raise Exception("Chord \"{}\" does not exist. Here is a list of all the possible chords:\n{}"
-                            .format(chord_type, types_of_chords.keys())) from None
-        self.chord = [self.bass_note + interval for interval in self.chord_intervals]
+    def __init__(self, bass_note, chord_type, custom_made=False, list_of_notes=[]):
+        if not custom_made:
+            self.bass_note = Note(bass_note) if isinstance(bass_note, str) else bass_note
+            self.chord_type = chord_type
+            try:
+                self.chord_intervals = types_of_chords[chord_type]
+            except KeyError:
+                raise Exception("Chord \"{}\" does not exist. Please create a custom chord with obj.custom_made if "
+                                "desired chord is not in the following list of all possible chords:\n{}"
+                                .format(chord_type, types_of_chords.keys())) from None
+            self.chord = [self.bass_note + interval for interval in self.chord_intervals]
+        else:
+            self.chord = []
+            for note in list_of_notes:
+                if isinstance(note, Note):
+                    self.chord.append(note)
+                elif isinstance(note, str):
+                    self.chord.append(Note(note))
+                else:
+                    raise Exception("{} must be a list of note/str objects.".format(list_of_notes))
+
+            self.bass_note = self.chord[0]
+            self.chord_type = "custom"
+
         self.max_inversions = len(self.chord) - 1
         self._index = -1
+
+
+    @classmethod
+    def custom_made(cls, list_of_notes: list):
+        cls.chord = []
+        for note in list_of_notes:
+            if isinstance(note, Note):
+                cls.chord.append(note)
+            elif isinstance(note, str):
+                cls.chord.append(Note(note))
+            else:
+                raise Exception("{} must be a list of note/str objects.".format(list_of_notes))
 
     def to_n_number_notes(self, number):
         new_notes = []
