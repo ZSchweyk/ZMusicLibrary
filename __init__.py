e3e711c21e3b53ef001453c4e4dfb51b42207881
Zeyn Schweyk <zschweyk@gmail.com>
2021-12-22 23:47:48 -0800

    Added UsefulFunctions, chord_types, and scale_types imports.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
 from .note import Note
 from .chord import Chord
 from .scale import Scale
+from .UsefulFunctions import *
+from .chord_types import types_of_chords
+from .scale_types import types_of_scales
+
